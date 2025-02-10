3c8e2d1898af13de8a0a2adfe546d082aeef4fc1
gregoriombp <gregpinheiro@outlook.com>
2025-02-10 12:29:12 -0300

    mudancas 6

--- public/js/home-script2.js
+++ public/js/home-script2.js
@@ -21,8 +21,17 @@ document.addEventListener("DOMContentLoaded", () => {
             const title = tooltipTitles[area.id] || "Área Interativa";
             tooltip.textContent = title;
             tooltip.style.display = "block";
-            tooltip.style.left = e.pageX + 10 + "px";
-            tooltip.style.top = e.pageY + 10 + "px";
+            
+            const container = document.querySelector('.containerhome');
+            const containerRect = container.getBoundingClientRect();
+            const offset = 5;
+            
+            // Calcula a posição relativa ao container
+            const relativeX = e.clientX - containerRect.left;
+            const relativeY = e.clientY - containerRect.top;
+            
+            tooltip.style.left = `${containerRect.left + relativeX + offset}px`;
+            tooltip.style.top = `${containerRect.top + relativeY + offset}px`;
         });
 
         area.addEventListener("mouseout", () => {
@@ -33,19 +42,19 @@ document.addEventListener("DOMContentLoaded", () => {
             if (area.id === "door1") {
                 window.location.href = "student-eixo1.html";
             } else if (area.id === "door2") {
-                window.location.href = "student-eixo1.html";
+                window.location.href = "#";
             } else if (area.id === "door3") {
-                window.location.href = "student-eixo1.html";
+                window.location.href = "#";
             } else if (area.id === "door4") {
-                window.location.href = "student-eixo1.html";
+                window.location.href = "#";
             } else if (area.id === "door5") {
-                window.location.href = "student-eixo1.html";
+                window.location.href = "#";
             } else if (area.id === "door6") {
-                window.location.href = "student-eixo1.html";
+                window.location.href = "#";
             } else if (area.id === "salaconferencia") {
-                window.location.href = "#";
+                window.location.href = "student-palestras.html";
             } else if (area.id === "espaco360") {
-                window.location.href = "#";
+                window.location.href = "student-auditorio.html";
             } else if (area.id === "character") {
                 return;
             }
